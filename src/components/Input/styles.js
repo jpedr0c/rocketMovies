cdc1063a36a666c3ce6f96708a6ba9d67379b136
jpedr0c <jpccarvalho2210@gmail.com>
2023-09-15 17:23:04 -0300

    style: modified padding and width

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,24 +1,26 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 40%;
+  width: 100%;
   display: flex;
   align-items: center;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
   color: ${({ theme }) => theme.COLORS.GRAY_100};
   border-radius: 1rem;
-  padding: 1.9rem 2.4rem;
+  padding: 1.9rem 1.6rem;
 
   > input {
     width: 100%;
-    border-radius: 1rem;
     border: none;
-    margin-left: 1.6rem;
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
     color: ${({ theme }) => theme.COLORS.WHITE_300};
 
     &::placeholder {
       color: ${({ theme }) => theme.COLORS.GRAY_100};
     }
+  }
+
+  > svg {
+    margin-right: 1rem;
   }
 `;

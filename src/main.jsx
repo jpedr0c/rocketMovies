d3dc44773f4ff0bee2c66cbafdb702c7818b6f59
jpedr0c <jpccarvalho2210@gmail.com>
2023-09-18 22:22:52 -0300

    test Add page

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme'
-import { Home } from './pages/Home';
+import { Add } from './pages/Add';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Add />
     </ThemeProvider>
   </React.StrictMode>,
 )
